e76455059e78f1320752551b3e811c707500b278
Rémy Schumm @kassandra/noser <remy.schumm@noser.com>
2018-11-13 09:48:34 +0100

    init

---
+++ webhallo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Das ist jetzt also PHP</title>
+</head>
+    <body>
+    <p>Das ist jetzt also PHP</p>
+    <?php
+        echo "Hallo Welt";
+	?>
+    </body>
+
+</html>
